39ae43922269d26754e5dc76ba3d4d658f604cba
Andrew Poženel - SDT <anderlli0053@live.com>
2022-05-06 19:54:11 +0200

    Add files via upload

---
+++ Main.cs
@@ -0,0 +1,29 @@
+using Godot;
+using System;
+using System.IO;
+using System.Linq;
+
+public class Main : Control
+{
+    // Declare member variables here. Examples:
+    // private int a = 2;
+    // private string b = "text";
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        
+    }
+
+    public void RandomizeWords(){
+
+        string[] words = {};
+
+    }
+
+//  // Called every frame. 'delta' is the elapsed time since the previous frame.
+//  public override void _Process(float delta)
+//  {
+//      
+//  }
+}
